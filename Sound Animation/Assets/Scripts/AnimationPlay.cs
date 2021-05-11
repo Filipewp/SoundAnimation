1d20eec29bd3253de0d1d4d5618835598cd764c5
Filipewp <filipepatricio@hotmail.com.br>
2021-05-11 15:26:03 -0300

    V1

---
+++ Sound Animation/Assets/Scripts/AnimationPlay.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationPlay : MonoBehaviour
+{
+    public Animator anim;
+
+    void Update()
+    {
+        anim.SetBool("Dance", true);
+    }
+
+}
